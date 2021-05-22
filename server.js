7d5c0f2a8a22aea3c14a94cd02d3a1272b598af6
El Roble <san_lucas2005@yahoo.com>
2021-05-22 16:52:31 -0600

    Add / route

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ app.get("/api/notes", (req, res) => {
   }
 })
 
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+)
+
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 )
